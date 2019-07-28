e887906a378260287720c44fc54df45622266892
Jamie Barton <jbarton10@email.arizona.edu>
2019-07-28 13:10:46 -0700

    Html is up dropdown buttons not working

---
+++ app/data/firends.js
@@ -0,0 +1,52 @@
+//Homemade data base for friends
+
+var friends = [{
+    name:"Ahmed",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    name:"Eric",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        3,
+        2,
+        1,
+        4,
+        4,
+        5,
+        2,
+        4,
+        4,
+        5
+      ]
+  },
+  {
+    name:"Jamie",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        1,
+        1
+      ]
+  }
+  
+  ]
