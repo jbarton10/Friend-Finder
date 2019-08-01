8d5730715d883aa8100df94578c847db37923c91
Jamie Barton <jbarton10@email.arizona.edu>
2019-07-31 18:40:42 -0700

    Object is being sent

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,49 +2,53 @@ var friends = require("./../data/friends");
 
 
 //Method for adding new persons to database, as well as comparing them to find closest match
-module.exports = function(app){
-    app.post("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.post("/api/friends", function (req, res) {
+        console.log("here");
         var newFriend = req.body;
+        console.log(newFriend);
         //Need to get friends from friends.js
-        
+
         friends.push(newFriend);
-        var closestMatch;
-        
-    
+        var closestFriendScore;
+        var closestFriend;
+
+
         //Maybe works?
-        for(var i = 0; i< friends.length; i++){
+        for (var i = 0; i < friends.length; i++) {
             var sumDifference;
-            for(var j = 0; j<friends[i].scores.length; j++){
+            for (var j = 0; j < friends[i].scores.length; j++) {
                 //Current score being compared
                 var current = friends[i].scores[i];
                 //The number that is the difference being added to the sum difference 
                 var placeholder = current - newFriend.scores[i];
                 //Adding to the sum difference
-                sumdifference +=placeholder;
+                sumDifference += placeholder;
             }
             //Setting the new closest match
-            if(sumDifference< closestMatch){
-                closestMatch =sumDifference;
+            if (sumDifference < closestFriendScore) {
+                closestFriendScore = sumDifference;
+                closestFriend = friends[i];
             }
         }
-      
-        res.json(closestMatch);
-      });
 
-
-
-  
+        res.json(closestFriend);
+    });
 
 
 
 
-        //Method for displaying the data
-        app.get("/api/friends", function(req, res){
-            return res.json(friends);
-        
-            });
-
-  
 
 
-        }
+
+
+    //Method for displaying the data
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+
+    });
+
+
+
+
+}
